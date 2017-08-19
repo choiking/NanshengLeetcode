c1eae5a16257766812f7808cc32165a596c0b0ce
choiking <497473650@qq.com>
2017-08-19 20:46:35 +0800

    added

---
+++ BinarySearch/reverse-words-in-a-string-ii.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public void reverseWords(char[] s) {
+        //3 step
+        //reverse the whole string
+        reverse(s, 0, s.length - 1);
+        //reverse each word
+        int start = 0;
+        for(int i = 0; i < s.length; i++) {
+            if(s[i] == ' ') {
+                reverse(s, start, i -1);
+                start = i + 1;
+            }
+        }
+        //reverse the last word. this will solve the corner case( only one word);
+        reverse(s, start, s.length - 1);
+    }
+    
+    public void reverse(char[] s, int start, int end) {
+    while (start < end) {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+    }
+}
---
+++ BinarySearch/rotate-array.java
@@ -0,0 +1,28 @@
+public class Solution {// 3 steps in place O(n) time 
+    public void rotate(int[] nums, int k) {
+            k = k % nums.length;
+        
+            reverse(nums, 0, nums.length - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, nums.length -1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start++] = nums[end];
+        nums[end--] = temp;
+    }
+}
+}
+
+public class Solution {// O(n) time, O(n) extra place
+    public void rotate(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
+}
