aba3166e4373d1f850794d2449ba407888ef2afe
choiking <497473650@qq.com>
2017-08-21 17:06:22 +0800

    added

---
+++ Permutation And Combination/subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        //Arrays.sort(nums);
+        dfs(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+    void dfs(List<List<Integer>> result, List<Integer> list, int[] nums, int pos) {
+        result.add(new ArrayList<>(list));
+        for (int i = pos; i < nums.length; i++) {
+            list.add(nums[i]);
+            dfs(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
