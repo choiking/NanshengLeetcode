3e9cd428e7d49c98000914b6ed079307330d0cd0
Nansheng Tu <tu.n@husky.neu.edu>
2017-09-27 16:15:28 -0400

    added

---
+++ Binary Tree-Divide and Conquer/binary-tree-inorder-traversal.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {// traversal
+    List<Integer> res = new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        inorderTraversal(root.left);
+        res.add(root.val);
+        inorderTraversal(root.right);
+        return res;
+    }
+}
