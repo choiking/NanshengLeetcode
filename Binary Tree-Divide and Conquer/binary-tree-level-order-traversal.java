af5ef43ea8334ec4de54fec3fda9d6cb462d6caf
Nansheng Tu <tu.n@husky.neu.edu>
2017-10-03 19:32:14 -0400

    added

---
+++ Binary Tree-Divide and Conquer/binary-tree-level-order-traversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList res = new ArrayList();
+        
+        if(root == null) {
+            return res;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            
+            //do the level
+            for (int i = 0; i < size; i++) {
+            //enqueue root.children
+            if(queue.peek().left != null) queue.offer(queue.peek().left);
+            if(queue.peek().right != null) queue.offer(queue.peek().right);
+            //dequeue 
+            level.add(queue.poll().val); 
+            }
+            
+            //add the level
+            res.add(level);
+        }
+        return res;
+    }
+}
