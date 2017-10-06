792a2ce66a2d4a3ab776f32f43d563b2649c0ead
Nansheng Tu <tu.n@husky.neu.edu>
2017-10-06 12:22:45 -0400

    revised

--- LinkedList/remove-duplicates-from-sorted-list-ii.java
+++ LinkedList/remove-duplicates-from-sorted-list-ii.java
@@ -28,3 +28,26 @@ class Solution {
         return dummy.next;
     }
 }
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr.next != null && curr.next.next != null) {
+            if (curr.next.val == curr.next.next.val) {
+                int val = curr.next.val;
+                while (curr.next != null && curr.next.val == val) {
+                curr.next = curr.next.next;
+                }
+            }
+            else {
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+}
