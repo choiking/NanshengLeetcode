fb36ea0b2c835a9194c123712ecb9b96277aaf35
Nansheng Tu <tu.n@husky.neu.edu>
2017-10-30 22:49:10 -0400

    added

---
+++ BFS/word-ladder.java
@@ -0,0 +1,55 @@
+/*
+Searching in Set is O(1), while searching in List is O(n);
+        h i t
+         /\
+        h o t
+       /\
+  d o t  l o t
+     /\     /\
+    d o g  l o g
+   /\ 
+  c o g
+one end BFS. 100ms running time.
+*/
+
+
+public int ladderLength(String b, String e, List<String> wordList) {
+    Set<String> dict = new HashSet<String>(wordList);
+        if(!dict.contains(e)){
+            return 0;
+        }
+
+    if(b.equals(e)) return 1;
+    
+    Queue<String> q = new LinkedList<String>();
+    q.add(b);
+
+    int level = 2;
+    
+    while(!q.isEmpty()) {
+        int sz = q.size();
+        
+        for(int i = 0; i < sz; i++) {
+            String tmp = q.poll();
+            //modify each letter of tmp
+            for(int j = 0; j< tmp.length(); j++) {
+                char[] chars = tmp.toCharArray();
+                
+                for(char c = 'a'; c <= 'z'; c++) {
+                    chars[j] = c;
+                    String tmp2 = new String(chars);
+                    // Found the end word
+                    if(tmp2.equals(e)) return level;
+                    // Put it to the queue
+                    if(dict.remove(tmp2)) {
+                        q.add(tmp2);
+                    }
+                }
+            }
+        }
+        
+        level++;
+    }
+    
+    return 0;
+}
